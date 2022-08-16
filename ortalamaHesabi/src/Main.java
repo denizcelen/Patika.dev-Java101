0f8ba2b1c2eb53407efd05927805353715eb48d9
denizcelen <110533039+denizcelen@users.noreply.github.com>
2022-08-16 19:29:24 +0300

    Add files via upload

---
+++ ortalamaHesabi/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Ortalama Hesaplama Programı");
+        System.out.print("DÖngünün sonlanacağı sayıyı giriniz: ");
+        int k= input.nextInt();
+        int toplam =0;
+        int sayi =0;
+        int i=1;
+
+
+        while(i <=k){
+            i++;
+            if(i %12==0){
+                sayi=sayi+i;
+                toplam++;
+            }
+
+        }
+        int orta= sayi / toplam;
+        if(sayi !=0){
+
+            System.out.println(k + "'ya kadadr olan 3'e ve 4'e bölünen sayıların ortalaması: "+ orta+ "dir." +sayi + " " + toplam);
+        }
+        else{
+            System.out.println(sayi + "'ye kadar 3'e 4e bölünen sayı yoktur'" );
+        }
+
+    }
+}
