c949f23a1016abee82afc74ca30877d329702fd0
denizcelen <110533039+denizcelen@users.noreply.github.com>
2022-11-14 21:48:50 +0300

    Add files via upload

---
+++ arrayMnMax/src/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        int [] list={15,12,788,1,-1,-788,2,0};
+        System.out.print("Girilen sayı: ");
+        int sayi=inp.nextInt();
+        Arrays.sort(list);
+        int min=list[0];
+        int max=list[7];
+        for(int i:list){
+            if(i>min&&i<sayi){
+                min=i;
+            }
+            if(i<max&&i>sayi){
+                max=i;
+            }
+        }
+        System.out.println("Girilen sayıdan küçük en yakın sayı : "+min);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : "+max);
+    }
+}
